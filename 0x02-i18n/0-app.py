1471cc92c5a4bccafc7ddf176779fb715826d907
kamau95 <linuskamau586@gmail.com>
2024-08-30 12:31:41 +0300

    flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -8,13 +8,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
-def page() -> str:
+@app.route('/', strict_slashes=False)
+def index() -> str:
     """
-    handle / route
+    Handles / route
     """
-    return render_template("0-index.html")
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port="5000", host="0.0.0.0", debug=True)
